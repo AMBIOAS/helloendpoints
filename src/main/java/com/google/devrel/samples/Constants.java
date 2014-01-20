fa72e30d00ac0155bc2acd7549ab93f98dde7d2d
Fred Pallesen <fred@strong.no>
2014-01-20 10:13:29 +0100

    inital changes
    this is from the Google sample

---
+++ src/main/java/com/google/devrel/samples/Constants.java
@@ -0,0 +1,17 @@
+package com.google.devrel.samples;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
+ */
+public class Constants {
+  public static final String WEB_CLIENT_ID = "873214469783-to0hsgvcp18qimkljdal85crtdo3av4m.apps.googleusercontent.com";
+  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+  public static final String API_EXPLORER_CLIENT_ID
+      = com.google.api.server.spi.Constant.API_EXPLORER_CLIENT_ID;
+}
+
+
